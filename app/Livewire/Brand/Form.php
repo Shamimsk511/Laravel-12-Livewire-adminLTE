4e5e40e352eecfaff4f73d99cce7a54d22ab1d9a
Shamim Reza <rahaman.mosaic@gmail.com>
2025-07-09 12:48:04 +0600

    initial commit

---
+++ app/Livewire/Brand/Form.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Brand;
+
+use Livewire\Component;
+
+class Form extends Component
+{
+    public function render()
+    {
+        return view('livewire.brand.form');
+    }
+}
